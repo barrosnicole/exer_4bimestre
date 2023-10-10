fb853b5690bef65b853fe18d27420d397cf5f043
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-10 10:31:02 -0300

    pronto2

--- 4ºbimestre/exer4.py
+++ 4ºbimestre/exer4.py
@@ -1,3 +1,13 @@
-numero=int(input("digite um número"))
-adiçao=numero(+1,+2,+3,+4,+5,+6,+7,+8,+9)
-print("a tabuada de adição do número {} é: {}".format(numero,adiçao))
+num=int(input("digite um número"))
+print('num{}*{}={}'.format(num,1,num*1))
+print('num{}*{}={}'.format(num,2,num*2))
+print('num{}*{}={}'.format(num,3,num*3))
+print('num{}*{}={}'.format(num,4,num*4))
+print('num{}*{}={}'.format(num,5,num*5))
+print('num{}*{}={}'.format(num,6,num*6))
+print('num{}*{}={}'.format(num,7,num*7))
+print('num {}*{}={}'.format(num,8,num*8))
+print('num{}*{}={}'.format(num,9,num*9))
+
+
+
