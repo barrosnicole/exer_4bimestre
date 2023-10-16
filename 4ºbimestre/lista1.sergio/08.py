697686bddbb9406ce187a520c391b104da3405b7
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-16 08:26:04 -0300

    b4

---
+++ 4ºbimestre/lista1.sergio/08.py
@@ -0,0 +1,4 @@
+dinheiro=float(input('quanto você ganha por hora?'))
+dias=int(input('quantos horas no mês você trabalha? '))
+salario=dinheiro*dias
+print('seu salário por mês é: {}'.format(salario))
