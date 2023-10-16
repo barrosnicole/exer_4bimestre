393e1850c27b74389fb2b50dff662e8ffd39a7ec
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-16 08:49:59 -0300

    b6

---
+++ 4ºbimestre/lista1.sergio/11.py
@@ -0,0 +1,12 @@
+n1= int(input('digite um numero:'))
+n2= int(input('digite outro numero:'))
+n3= float(input('digite o terceiro numero:'))
+c1=(n1*2)*(n2/2)
+c2=(n1*3)+n3
+c3=(n3)**3
+print('o produto do dobro do primeiro com metade do segundo é: {}'.format(c1))
+print('a soma do trilo do primeiro com o terceiro é: {}'.format(c2))
+print('o terceiro elevado ao cubo é:{} '.format(c3))
+      
+      
+    
