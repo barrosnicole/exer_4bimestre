2f8f518ac1063b151c35c68ded6e49a322524aa7
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-16 08:23:03 -0300

    b3

---
+++ 4ºbimestre/lista1.sergio/07.py
@@ -0,0 +1,5 @@
+q=float(input('digite um numero, ele sera a medida do lado de um quadrado: '))
+a=q*q
+d=a*2
+print('a area do quadrado pela medida disposta {}, é: {}'.format(q,a))
+print('e o dobro dessa area é:{} '.format(d))
