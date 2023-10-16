2a516b2cc2091453cb6c6c9f9f14ee2a87b853cc
CrislainyMartins <143403752+CrislainyMartins@users.noreply.github.com>
2023-10-16 08:18:53 -0300

    b2

---
+++ 4ºbimestre/lista1.sergio/06.py
@@ -0,0 +1,5 @@
+d=float(input('digite um numero, ele sera o dimetro de um circulo'))
+r=d/2
+print('o raio desse circulo pelo diametro {}apresentado é {}'.format(d,r))
+a=3.14*r**(2)
+print('já sua área é: {}'.format(a))
